1e196a110c0ed0512d53c227dfca31233cca9292
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-12-24 21:46:07 +0300

    fix error in home

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -3,22 +3,6 @@ import classNames from 'classnames'
 
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
-// import { Button } from '@/ui'
-import { Traffic } from '@/modules/traffic'
-import { Introduce } from '@/modules/introduce'
-import { Faq } from '@/modules/faq'
-import { Gumbit } from '@/modules/gumbit'
-// import Link from 'next/link'
-import { Mission } from '@/modules/mission'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { Part } from '@/modules/part'
-import { Company } from '@/modules/company'
-import { WindowFiller } from '@/modules/windowFiller'
-import { Principle } from '@/modules/principle'
-
-gsap.registerPlugin(ScrollTrigger, useGSAP)
 
 const Home: FC<HomeProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
