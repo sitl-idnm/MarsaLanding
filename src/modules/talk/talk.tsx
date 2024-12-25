1459203fe146fc3c4fbfd5ec0a7180dd44035ccd
sitl-idnm <klevtsevitch.lev@yandex.ru>
2024-12-25 05:18:10 +0300

    Сверстал блоки Интро , Разговор , Кейсы

---
+++ src/modules/talk/talk.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react'
+import classNames from 'classnames'
+
+import styles from './talk.module.scss'
+import { TalkProps } from './talk.types'
+import TitleGradient from '@/ui/titleGradient/titleGradient'
+import Image from 'next/image'
+import AvatarFirst from '@public/images/avatarFirst.png'
+import AvatarSecond from '@public/images/avatarSecond.png'
+import { ButtonBlue } from '@/ui'
+
+
+const Talk: FC<TalkProps> = ({
+  className
+}) => {
+  const rootClassName = classNames(styles.root, className)
+
+  return (
+    <div className={rootClassName}>
+      <h2 className={styles.title}>
+        <TitleGradient>
+          Что говорят о нас другие арбитражники?
+        </TitleGradient>
+      </h2>
+      <div className={styles.content}>
+        <div className={styles.item}>
+          <Image className={styles.itemImage} src={AvatarFirst} alt=''></Image>
+          <div className={styles.itemText}>
+            <h3 className={styles.itemTitle}>
+              Александр Т.
+            </h3>
+            <p className={styles.itemDescription}>
+              Действительно полезный канал! За 2 недели увеличил ROI на 50%.
+            </p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <Image className={styles.itemImage} src={AvatarFirst} alt=''></Image>
+          <div className={styles.itemText}>
+            <h3 className={styles.itemTitle}>
+              Мария К.
+            </h3>
+            <p className={styles.itemDescription}>
+              Здесь все по делу: инструменты, стратегии, офферы. Не трачу время на поиски            </p>
+          </div>
+        </div>
+      </div>
+      <ButtonBlue>
+        Присоединяйтесь к нашей команде!
+      </ButtonBlue>
+    </div>
+  )
+}
+
+export default Talk
--- src/ui/buttonBlue/buttonBlue.tsx
+++ src/ui/buttonBlue/buttonBlue.tsx
@@ -8,16 +8,31 @@ import styles from './buttonBlue.module.scss';
 import { ButtonBlueProps } from './buttonBlue.types';
 import Link from 'next/link';
 import Tg from '@icons/tg_blue.svg'
+import TgHover from '@icons/tg_blue_hover.svg'
 
 gsap.registerPlugin(ScrollTrigger);
 
 const ButtonBlue: FC<ButtonBlueProps> = ({ children, blue }) => {
 
   const backgroundUrl = blue ? '/images/back_button_blue.png' : '/images/back_button.png';
+  const hoverBackgroundUrl = blue ? '/images/back_button_blue_hover.png' : '/images/back_button_blue_hover.png';
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.style.backgroundImage = `url(${hoverBackgroundUrl})`;
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.style.backgroundImage = `url(${backgroundUrl})`;
+  };
 
   return (
     <Link href='#'>
-      <div className={styles.button} style={{ backgroundImage: `url(${backgroundUrl})` }}>
+      <div
+        className={styles.button}
+        style={{ backgroundImage: `url(${backgroundUrl})` }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <Tg />
         <span className={styles.text}>{children}</span>
       </div>
